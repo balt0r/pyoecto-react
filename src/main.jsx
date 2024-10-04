3aec6d64cb237c1909bccf730ed5b39a62fdf2d3
dante perrone <dantep2015@gmail.com>
2024-10-03 21:24:47 -0300

    arregle el codigo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from './App.jsx';  // Asegúrate de que esta ruta sea correcta
 
-createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
